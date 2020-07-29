ae4a33591c9c30e35356c74b528877c4f96bd0d9
WhizSid <whizsid@aol.com>
2020-07-30 01:50:25 +0530

    Basic functionality implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,34 @@
 use std::io::{Error, ErrorKind};
 use std::ops::Sub;
+use std::convert::TryFrom;
 
 #[derive(Clone)]
 struct BinImage {
     pixels: Vec<Vec<bool>>,
 }
 
+#[derive(Clone)]
 enum SubIter {
     First,
     Second,
 }
 
 impl BinImage {
-    pub fn new(width: usize, height: usize) -> BinImage {
+    /// Creating a new image for a given width and height
+    pub fn new(width: usize, height: usize, fill_color: bool) -> BinImage {
         let mut pixels: Vec<Vec<bool>> = vec![];
 
-        for i in 0..height {
+        for _ in 0..height {
             let mut row: Vec<bool> = vec![];
 
-            for j in 0..width {
-                row.push(false);
+            for _ in 0..width {
+                row.push(fill_color);
             }
 
             pixels.push(row);
         }
 
         BinImage { pixels }
-    }
-
-    pub fn from_pixels(pixels: Vec<Vec<bool>>) -> Result<BinImage, Error> {
-        let mut first = true;
-        let mut width = 0;
-        for row in pixels.iter() {
-            if first {
-                width = row.len();
-            } else {
-                if width != row.len() {
-                    return Err(Error::new(ErrorKind::InvalidInput, "Invalid image"));
-                }
-            }
-        }
-
-        Ok(BinImage { pixels })
     }
 
     pub fn get_width(&self) -> usize {
@@ -68,7 +55,7 @@ impl BinImage {
     pub fn set_value(&mut self, x: usize, y: usize, value: bool) -> Result<(), Error> {
         match self.pixels.get(y) {
             Some(row) => match row.get(x) {
-                Some(val) => {
+                Some(_) => {
                     self.pixels[y][x] = value;
                     Ok(())
                 }
@@ -115,11 +102,87 @@ impl BinImage {
     }
 }
 
+struct BinImageIntoIter {
+    bin_image: BinImage,
+    x: usize,
+    y: usize
+}
+
+impl IntoIterator for BinImage {
+    type Item = (usize, usize, bool);
+    type IntoIter = BinImageIntoIter;
+
+    fn into_iter(self)->BinImageIntoIter {
+        BinImageIntoIter {
+            bin_image: self,
+            x: 0,
+            y: 0
+        }
+    }
+}
+
+impl Iterator for BinImageIntoIter {
+   type Item = (usize, usize, bool);
+
+   fn next(&mut self)->Option<(usize, usize, bool)> {
+        
+       let height = self.bin_image.get_height();
+       let width = self.bin_image.get_width();
+
+       if height>=self.y && width >= self.x {
+            return None;
+       }
+
+       let ret = (self.x, self.y, self.bin_image.get_value(self.x, self.y).unwrap());
+
+       if self.x>= width {
+        self.y +=1;
+        self.x =0;
+       } else {
+        self. x += 1;
+       }
+
+       Some(ret)
+   }
+}
+
+impl TryFrom<Vec<Vec<bool>>> for BinImage {
+    type Error = Error;
+
+    fn try_from(pixels: Vec<Vec<bool>>)->Result<BinImage, Error>{
+        let mut first = true;
+        let mut width = 0;
+        for row in pixels.iter() {
+            if first {
+                width = row.len();
+            } else {
+                if width != row.len() {
+                    return Err(Error::new(ErrorKind::InvalidInput, "Invalid image"));
+                }
+            }
+            first = false;
+        }
+
+        Ok(BinImage { pixels })
+
+    }
+}
+
 impl Sub<BinImage> for BinImage {
     type Output = BinImage;
 
     fn sub(self, rhs: BinImage)->BinImage {
-        unimplemented!()
+        let mut new_img = self.clone();
+        
+        let iter = rhs.into_iter();
+
+        for (x,y,val) in iter {
+            if val {
+                let _result = new_img.set_value(x,y, false); 
+            }
+        }
+
+        new_img
     }
 }
 
@@ -150,12 +213,46 @@ fn calculate_ap_and_bp(
     (a_p, b_p)
 }
 
-pub fn recursive(pixels: Vec<Vec<bool>>, c: usize) -> (Vec<Vec<bool>>, usize) {
-    unimplemented!()
+fn recursive(image: BinImage, c: usize) -> (BinImage, usize) {
+    // Searching for M that satisfied the first subiteration
+    let mut c = c; 
+  
+    let mut image_mut = image;
+    let sub_iters = vec!(SubIter::First, SubIter::Second);
+
+    for sub_iter in sub_iters {
+    
+        let mut m = BinImage::new(image_mut.get_width(), image_mut.get_height(), false);
+        let img_iter = image_mut.clone().into_iter();
+
+        for (x,y,_) in img_iter {
+            if image_mut.sub_iter(sub_iter.clone(), x, y) {
+                let _result = m.set_value(x, y, true);
+                c +=1;
+            }
+        }
+
+        image_mut = image_mut - m;
+
+        if c == 0 {
+            return (image_mut, c);
+        }
+
+    }
+
+    return recursive(image_mut, c);
 }
 
-pub fn imgthin(pixels: Vec<Vec<bool>>) -> Vec<Vec<bool>> {
-    let (thinned, _) = recursive(pixels, 0);
+pub fn imgthin(pixels: Vec<Vec<bool>>) -> Result<Vec<Vec<bool>>, Error> {
+    let bin_image_result = BinImage::try_from(pixels);
 
-    unimplemented!();
+    match bin_image_result {
+        Ok(bin_image)=>{
+            let (thinned, _) = recursive(bin_image, 0);
+
+            Ok(thinned.get_pixels())
+
+        }
+        Err(e)=>Err(e)
+    }
 }
